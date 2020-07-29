7c7025160561a80c8ba44b748019f59b68746d81
Viper5077 <56239073+Viper5077@users.noreply.github.com>
2020-07-29 14:59:39 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,35 +1,91 @@
+//Main start over function - resets all values
 function startOver() {
   currentTab = 0
-  currentTab1 = 0
-  currentTab2 = 0
   leaveStartDate = ""
   conusOrOconus = ""
   earnedDays = 0
   retirementDate = ""
   ptdy = 0
   terminalLeave = 0
-  seperationDate = ""
+  separationDate = ""
   extraLeaveDays = 0
-  dateArray = []
-  var tab = document.getElementsByClassName("tab")
-  for (i = 0; i < tab.length; i++) {
-    tab[i].style.display = "none"
+  input = ""
+  var retirementTab = document.getElementsByClassName("retirement-tab")
+  for (i = 0; i < retirementTab.length; i++) {
+    retirementTab[i].style.display = "none"
   }
-  var tab1 = document.getElementsByClassName("tab1")
-  for (i = 0; i < tab1.length; i++) {
-    tab1[i].style.display = "none"
+  var separationTab = document.getElementsByClassName("separation-tab")
+  for (i = 0; i < separationTab.length; i++) {
+    separationTab[i].style.display = "none"
   }
-  var tab2 = document.getElementsByClassName("tab2")
-  for (i = 0; i < tab2.length; i++) {
-    tab2[i].style.display = "none"
+  var involuntaryseparationTab = document.getElementsByClassName("involuntary-separation-tab")
+  for (i = 0; i < involuntaryseparationTab.length; i++) {
+    involuntaryseparationTab[i].style.display = "none"
   }
   document.getElementById("situation").style.display = "block"
   document.getElementById("progressBar").style.width = "0%"
-  document.getElementById("enlistmentDate").value = ""
-  document.getElementById("plannedRetireDate").value = ""
-  document.getElementById("leaveInput").value = ""
-  document.getElementById("leaveInput1").value = ""
-  document.getElementById("leaveInput2").value = ""
-  document.getElementById("plannedSeperateDate").value = ""
-  document.getElementById("plannedSeperateDate2").value = ""
+  var elements = document.querySelectorAll("#extraLeaveDays, #enlistmentDate, #plannedRetireDate, #leaveInput, #leaveInput1, #leaveInput2, #plannedSeparateDate, #plannedInvSeparateDate")
+  for (i = 0; i < elements.length; i++) {
+    elements[i].value = ""
+  }
+  var elements = document.querySelectorAll("#no, #yes-extraLeave, #yes-ptdy, #oconus, #conus")
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.borderColor = "transparent"
+  }
+  document.getElementById("extraLeaveDays").style.display = "none"
+}
+//Event Listeners
+document.getElementsByClassName("leaveInput")[0].addEventListener("input", function () {
+  document.getElementsByClassName("daysAlreadyEarned")[0].value = ""
+})
+document.getElementsByClassName("leaveInput")[1].addEventListener("input", function () {
+  document.getElementsByClassName("daysAlreadyEarned")[1].value = ""
+})
+document.getElementsByClassName("leaveInput")[2].addEventListener("input", function () {
+  document.getElementsByClassName("daysAlreadyEarned")[2].value = ""
+})
+document.getElementsByClassName("daysAlreadyEarned")[0].addEventListener("input", function () {
+  document.getElementsByClassName("leaveInput")[0].value = ""
+})
+document.getElementsByClassName("daysAlreadyEarned")[1].addEventListener("input", function () {
+  document.getElementsByClassName("leaveInput")[1].value = ""
+})
+document.getElementsByClassName("daysAlreadyEarned")[2].addEventListener("input", function () {
+  document.getElementsByClassName("leaveInput")[2].value = ""
+})
+
+
+
+
+//Other functions used for calculation  
+function calculateEarnedDays(d1, d2) {
+  earnedDays = Math.floor(monthDiff(d1, d2) * 2.5)
+  return earnedDays
+}
+function monthDiff(d1, d2) {
+  var months;
+  months = (d2.getFullYear() - d1.getFullYear()) * 12;
+  months -= d1.getMonth();
+  months += d2.getMonth();
+  return months <= 0 ? 0 : months;
+}
+
+//Navigation
+function showTab(n, x) {
+  x[n].style.display = "block";
+  var percentage = `${Math.floor(100 / x.length * currentTab + 100 / x.length)}%`
+  document.getElementById("progressBar").style.width = percentage
+  document.getElementById("percentage").innerHTML = percentage
+}
+function nextPrev(n) {
+  var x
+  if (event.srcElement.parentElement.parentElement.className.includes("tab")) {
+    x = document.getElementsByClassName(event.srcElement.parentElement.parentElement.classList[0])
+  } else {
+    x = document.getElementsByClassName(event.srcElement.parentElement.parentElement.parentElement.classList[0])
+  }
+  console.log(x)
+  x[currentTab].style.display = "none";
+  currentTab = currentTab + n;
+  showTab(currentTab, x);
 }
--- js/retire.js
+++ js/retire.js
@@ -1,121 +1,97 @@
-var leaveStartDate, conusOrOconus, earnedDays, retirementDate, ptdy, terminalLeave
-var dateArray = []
+//initialize variables
+var leaveStartDate, conusOrOconus, earnedDays, retirementDate, ptdy, terminalLeave, input
 var currentTab = 0
 var calculateBtn = document.getElementsByClassName("calculate")[0]
 var progressPercetage = document.getElementById("percentage")
+var todaysDate = new Date()
 
-calculateBtn.addEventListener("click", calculation)
+document.getElementsByClassName("today")[0].value = new Date().toISOString().substr(0, 10)
+document.getElementById("progressBar").style.width = "0%"
+
+//Main Calculation
+function retirementCalculation() {
+  terminalLeave = 0
+  if (conusOrOconus == "" || (document.getElementById("plannedRetireDate").value == "" && document.getElementById("enlistmentDate").value == "")) {
+    document.getElementsByClassName("calculated-output")[0].innerHTML = `Please make sure you complete all parts of the form.`
+  } else {
+    if (conusOrOconus == "conus") {
+      ptdy = 20
+    } else if (conusOrOconus == "oconus") {
+      ptdy = 30
+    }
+    if (document.getElementsByClassName("leaveInput")[0].value == "" && document.getElementsByClassName("daysAlreadyEarned")[0].value == "") {
+      terminalLeave += 60
+    } else if (document.getElementsByClassName("daysAlreadyEarned")[0].value !== "") {
+      terminalLeave += +calculateEarnedDays(new Date(`${todaysDate.getFullYear()}-${todaysDate.getMonth() + 2}-1`), new Date(`${+retirementDate.getFullYear()}-${+retirementDate.toISOString().substr(5, 2)}-1`))
+      terminalLeave += +document.getElementsByClassName("daysAlreadyEarned")[0].value
+    } else if (document.getElementsByClassName("leaveInput")[0].value !== "") {
+      terminalLeave += +document.getElementsByClassName("leaveInput")[0].value
+    }
+    terminalLeave += +calculateEarnedDays(new Date(`${+retirementDate.getFullYear() - 1}-10-1`), new Date(`${+retirementDate.getFullYear()}-${+retirementDate.toISOString().substr(5, 2)}-1`))
+    leaveStartDate.setDate(leaveStartDate.getDate() - (terminalLeave + +ptdy))
+    var leaveStartMonth = leaveStartDate.toLocaleString('default', { month: 'long' });
+    var retirementMonth = retirementDate.toLocaleString('default', { month: 'long' });
+    document.getElementsByClassName("calculated-output")[0].innerHTML = `With a final retirement date of ${retirementMonth} ${retirementDate.getDate()}, ${retirementDate.getFullYear()} (${retirementDate.toLocaleDateString('en-US')}), you should start terminal leave on ${leaveStartMonth} ${leaveStartDate.getDate()}, ${leaveStartDate.getFullYear()} (${leaveStartDate.toLocaleDateString('en-US')}). This assumes ${ptdy} days of PTDY and ${terminalLeave} days of terminal leave, including the leave earned while on terminal leave (${earnedDays}).`
+  }
+}
+
+function conus() {
+  conusOrOconus = event.srcElement.id
+  event.srcElement.style.borderColor = "#33466e"
+  if (event.srcElement.id == "conus") {
+    document.getElementById("oconus").style.borderColor = "transparent"
+  } else {
+    document.getElementById("conus").style.borderColor = "transparent"
+  }
+}
+
+//Event listeners
+document.getElementById("plannedRetireDate").addEventListener("change", function () {
+  document.getElementById("enlistmentDate").value = ""
+  input = new Date(this.value)
+  if (+input.getDate() !== 1) {
+    input.setDate(1)
+    input.setMonth(+input.getMonth() + 1)
+  }
+  retirementDate = new Date(`${input.getFullYear()}-${input.toISOString().substr(5, 2)}-${input.getDate()}`);
+  leaveStartDate = new Date(`${input.getFullYear()}-${input.toISOString().substr(5, 2)}-${input.getDate()}`);
+});
+document.getElementById("enlistmentDate").addEventListener("change", function () {
+  document.getElementById("plannedRetireDate").value = ""
+  input = new Date(this.value);
+  input.setFullYear(+input.getFullYear() + 20)
+  if (+input.getDate() !== 1) {
+    input.setDate(1)
+    input.setMonth(+input.getMonth() + 1)
+  }
+  retirementDate = new Date(`${input.getFullYear()}-${input.toISOString().substr(5, 2)}-${input.getDate()}`);
+  leaveStartDate = new Date(`${input.getFullYear()}-${input.toISOString().substr(5, 2)}-${input.getDate()}`);
+});
+document.getElementsByClassName("leaveInput")[0].addEventListener("input", function () {
+  document.getElementsByClassName("daysAlreadyEarned")[0].value = ""
+})
+document.getElementsByClassName("today")[0].addEventListener("change", function () {
+  todaysDate = document.getElementsByClassName("today")[0].value
+})
+calculateBtn.addEventListener("click", retirementCalculation)
 document.getElementById("retire").addEventListener("click", function () {
   document.getElementById("situation").style.display = "none"
-  showTab(0)
+  showTab(0, document.getElementsByClassName("retirement-tab"))
 });
-document.getElementById("progressBar").style.width = "0%"
-document.getElementById("home").addEventListener("click", function () {
+document.getElementsByClassName("home")[0].addEventListener("click", function () {
   currentTab = 0
-  var tab = document.getElementsByClassName("tab")
+  var tab = document.getElementsByClassName("retirement-tab")
   for (i = 0; i < tab.length; i++) {
     tab[i].style.display = "none"
   }
   document.getElementById("situation").style.display = "block"
   document.getElementById("progressBar").style.width = "0%"
 })
-function showTab(n) {
-  var x = document.getElementsByClassName("tab");
-  x[n].style.display = "block";
-  if (n == 0) {
-    progressPercetage.innerHTML = "25%"
-    document.getElementById("progressBar").style.width = "25%"
-  }
-  if (n == 1) {
-    progressPercetage.innerHTML = "50%"
-    document.getElementById("progressBar").style.width = "50%"
-  }
-  if (n == 2) {
-    progressPercetage.innerHTML = "75%"
-    document.getElementById("progressBar").style.width = "75%"
-  }
-  if (n == 3) {
-    progressPercetage.innerHTML = "100%"
-    document.getElementById("progressBar").style.width = "100%"
-  }
-}
-function nextPrev(n) {
-  var x = document.getElementsByClassName("tab");
-  x[currentTab].style.display = "none";
-  currentTab = currentTab + n;
-  showTab(currentTab);
-}
-document.getElementById("plannedRetireDate").addEventListener("change", function () {
-  var input = this.value;
-  var array = input;
-  array = array.split("-");
-  dateArray[0] = +array[0]
-  if (+array[2] == 1) {
-    dateArray[1] = +array[1]
-    dateArray[2] = +array[2]
-  } else {
-    dateArray[1] = +array[1] + 1
-    dateArray[2] = +array[2] - (+array[2] - 1)
-  }
-  retirementDate = new Date(dateArray)
-  leaveStartDate = new Date(dateArray);
-});
-document.getElementById("enlistmentDate").addEventListener("change", function () {
-  var input = this.value;
-  var array = input;
-  array = array.split("-");
-  dateArray[0] = +array[0] + 20
-  if (+array[2] == 1) {
-    dateArray[1] = +array[1]
-    dateArray[2] = +array[2]
-  } else {
-    dateArray[1] = +array[1] + 1
-    dateArray[2] = +array[2] - (+array[2] - 1)
-  }
-  retirementDate = new Date(dateArray)
-  leaveStartDate = new Date(dateArray);
-});
-function conus() {
-  conusOrOconus = event.srcElement.id
-}
-function calculation() {
-  terminalLeave = 0
-  if (conusOrOconus == "" || (document.getElementById("plannedRetireDate").value == "" && document.getElementById("enlistmentDate").value == "")) {
-    document.getElementById("calculatedAmount").innerHTML = `Please make sure you complete all parts of the form.`
-  } else {
-    if (conusOrOconus == "conus") {
-      terminalLeave += 20
-      ptdy = 20
-    } else if (conusOrOconus == "oconus") {
-      terminalLeave += 30
-      ptdy = 30
-    }
-    if (document.getElementById("leaveInput").value == "") {
-      terminalLeave += 60
-    } else {
-      terminalLeave += +document.getElementById("leaveInput").value
-    }
-    terminalLeave += +calculateEarnedDays(new Date(`${+dateArray[0] - 1}-10-1`), new Date(`${+dateArray[0]}-${+dateArray[1]}-1`))
-    leaveStartDate.setDate(leaveStartDate.getDate() - terminalLeave)
-    var month = leaveStartDate.toLocaleString('default', { month: 'long' });
-    var month2 = retirementDate.toLocaleString('default', { month: 'long' });
-    document.getElementById("calculatedAmount").innerHTML = `With a final retirement date of ${month2} ${retirementDate.getDate()}, ${retirementDate.getFullYear()} (${retirementDate.toLocaleDateString('en-US')}), you should start terminal leave on ${month} ${leaveStartDate.getDate()}, ${leaveStartDate.getFullYear()} (${leaveStartDate.toLocaleDateString('en-US')}). This assumes ${ptdy} days of PTDY and ${terminalLeave} days of terminal leave, including the leave earned while on terminal leave (${earnedDays}).`
-  }
-}
-function monthDiff(d1, d2) {
-  var months;
-  months = (d2.getFullYear() - d1.getFullYear()) * 12;
-  months -= d1.getMonth();
-  months += d2.getMonth();
-  return months <= 0 ? 0 : months;
-}
-function calculateEarnedDays(d1, d2) {
-  earnedDays = Math.floor(monthDiff(d1, d2) * 2.5)
-  return earnedDays
-}
+document.getElementsByClassName("daysAlreadyEarned")[0].addEventListener("input", function () {
+  document.getElementsByClassName("leaveInput")[0].value = ""
+})
 document.getElementsByClassName("reset")[0].addEventListener("click", function () {
-  retirementDate = new Date(dateArray)
-  leaveStartDate = new Date(dateArray);
+  retirementDate = input
+  leaveStartDate = input
   terminalLeave = 0
 })
-
